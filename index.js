1c492d790226feb53d106281a563824bfc9a7b53
Ather1230 <ather.biswas0057@gmail.com>
2022-11-10 17:42:31 +0600

    server updated

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -11,79 +11,89 @@ app.use(express.json());
 
 //mongodb cdn
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.SECURITY_KEY}@cluster0.ukuxru5.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-async function run(){
-    try{
-        const serviceCollection = client.db('e-service90DbUser').collection('services');
-        const reviewCollection = client.db('e-service90DbUser').collection('reviews');
+async function run() {
+  try {
+    const serviceCollection = client
+      .db("e-service90DbUser")
+      .collection("services");
+    const reviewCollection = client
+      .db("e-service90DbUser")
+      .collection("reviews");
 
-        app.get('/services', async(req, res) => {
-            const query = {};
-            const cursor = serviceCollection.find(query);
-            const services = await cursor.limit(3).toArray();
-            res.send(services);
-        });
-        app.get('/allServices', async(req, res) => {
-            const query = {};
-            const cursor = serviceCollection.find(query);
-            const services = await cursor.toArray();
-            res.send(services);
-        });
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.limit(3).toArray();
+      res.send(services);
+    });
+    app.get("/allServices", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
 
-        app.get('/services/:id', async(req, res) => {
-            const id = req.params.id;
-            const query = {_id: ObjectId(id)};
-            const service = await serviceCollection.findOne(query);
-            res.send(service);
-        });
-        app.get('/checkout/:id', async(req, res) => {
-            const id = req.params.id;
-            const query = {_id: ObjectId(id)};
-            const service = await serviceCollection.findOne(query);
-            res.send(service);
-        });
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+    app.get("/checkout/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
 
-        app.get('/reviews', async(req, res) => {
-            let query = {};
-            if(req.query.email){
-                query = {
-                    email: req.query.email
-                }
-            }
-            const cursor = reviewCollection.find(query);
-            const reviews = await cursor.toArray();
-            res.send(reviews)
-        });
+    app.get("/reviews", async (req, res) => {
+      let query = {};
+      if (req.query.email) {
+        query = {
+          email: req.query.email,
+        };
+      }
+      const cursor = reviewCollection.find(query);
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    });
 
-        app.post('/reviews', async(req, res) =>{
-            const review = req.body;
-            const result = await reviewCollection.insertOne(review);
-            res.send(result)
-        });
+    app.post("/reviews", async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      res.send(result);
+    });
 
-        app.delete('/reviews/:id', async(req, res) => {
-            const id = req.params.id;
-            const query = {_id: ObjectId(id)};
-            const result = await reviewCollection.deleteOne(query);
-            res.send(result)
-        })
+    app.get("/servicesRevew/:id", async (req, res) => {
+      const revid = req.params.id;
+      
+      const query = {service:revid};
+      const result = await reviewCollection.findOne(query);
+     
+      res.send(result)
+    });
 
-        
-    }
-    finally{
-        
-    }
+    app.delete("/reviews/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await reviewCollection.deleteOne(query);
+      res.send();
+    });
+  } finally {
+  }
 }
-run().catch(error => console.error(error))
+run().catch((error) => console.error(error));
 
-
-
-
-app.get('/', (req, res) => {
-    res.send('E-service90 server is running')
+app.get("/", (req, res) => {
+  res.send("E-service90 server is running");
 });
 
 app.listen(port, () => {
-    console.log(`E-service90 running on : ${port}`)
-})
+  console.log(`E-service90 running on : ${port}`);
+});
